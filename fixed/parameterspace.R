13cf14e2985d041c92187077b03d85a7c4be306d
Matthias Gruber <matthias.gruber@outlook.com>
2020-01-27 11:03:15 +0100

    .

--- fixed/parameterspace.R
+++ fixed/parameterspace.R
@@ -5,13 +5,13 @@ ps = makeParamSet(
   makeDiscreteParam("surrogate", values = c("regr.km","regr.randomForest")),
   
   makeDiscreteParam("kernel", values = c("powexp","gauss","matern5_2", "matern3_2"),
-                    requires = quote(Surrogate == "regr.km")),
+                    requires = quote(surrogate == "regr.km")),
   
   makeIntegerParam("nodesize", lower = 2, upper = 7,
-                   requires = quote(Surrogate == "regr.randomForest")),
+                   requires = quote(surrogate == "regr.randomForest")),
   
   makeIntegerParam("mtry", lower = 1, upper = 3,
-                   requires = quote(Surrogate == "regr.randomForest")), 
+                   requires = quote(surrogate == "regr.randomForest")), 
   
   makeDiscreteParam("infillCrit", values = c("makeMBOInfillCritEI",
                                              "makeMBOInfillCritEIcontrolExploration",
@@ -21,13 +21,13 @@ ps = makeParamSet(
                                              "makeMBOInfillCritAdaCB")),
   
   makeNumericParam("controlExploration", lower = 0.008, upper = 0.015,
-                   requires = quote(InfillCrit == "makeMBOInfillCritEIcontrolExploration")),
+                   requires = quote(infillCrit == "makeMBOInfillCritEIcontrolExploration")),
   
   makeNumericParam("startControlExploration", lower = 0.008, upper = 0.03,
-                   requires = quote(InfillCrit == "makeMBOInfillCritAdaEIctrlExploration")),
+                   requires = quote(infillCrit == "makeMBOInfillCritAdaEIctrlExploration")),
   
   makeNumericParam("endControlExploration", lower = 0.0008, upper = 0.002,
-                   requires = quote(InfillCrit == "makeMBOInfillCritAdaEIctrlExploration")),
+                   requires = quote(infillCrit == "makeMBOInfillCritAdaEIctrlExploration")),
   
   makeIntegerParam("amountInitialDesign", lower = 9, upper = 30),
   
