b48ccfc87c76323334efd4a3c76fceaad7438856
ascheppach <56842416+ascheppach@users.noreply.github.com>
2020-03-14 10:01:16 +0100

    Add files via upload

--- R/optimizeSpot.R
+++ R/optimizeSpot.R
@@ -27,89 +27,23 @@ for (i in 1:n) {
 set.seed(345)
 
 
-                         ############## SPOT ##################
-##### ES-Optimizer
-
-# define objective function / simulation
-model <- randomForest(data = data, x = data[,1:3], y = data$ratio)
-fun = function(x) {
-  predict(model, newdata = x)
-}
-
-
-# define optimizer / algo
-fun2 = function(xmat) {
-  xmat = as.data.frame(t(xmat))
-  colnames(xmat) <- c("nu", "mue")
-  optimES(fun = fun, lower = c(10,500,0), upper = c(5555,20210,1000), 
-              control = list(funEvals = 50, nu = xmat$nu, mue = xmat$mue))$ybest
-}
-
-fun3 = function(xmat) {
-  apply(xmat, 1, fun2)
-}
-
-# define metamodel and run spot
-res <- spot(fun = fun3, lower = c(3,5), upper = c(5,15),
-            control = list(funevals = 100))
 
 
 
-res <- spot(fun = fun, lower = c(10,500,0), upper = c(5555,20210,1000))
-
-
-
-##### Genoud-Optimizer -> muss nicht optimiert werden, weil nur populationsize als parameter
-##### logischerweise ist er umso höher, je höher populationsize ist
-# define objective function / simulation
-model <- randomForest(data = data, x = data[,1:3], y = data$ratio)
-fun = function(x) {
-  predict(model, newdata = x)
-}
-
-# define optimizer / algo
-fun2 = function(x) {
-  optimGenoud(fun = fun, lower = c(10,500,0), upper = c(5555,20210,1000), 
-              control = list(funEvals = 50, populationSize = x))$ybest
-}
-
-fun3 = function(xmat){
-  apply(xmat,1,fun2)
-}
-
-# define metamodel and run spot
-res <- spot(fun = fun3, lower = c(20), upper = c(40), 
-            control = list(funevals = 100))
-
-res$ybest
-res$xbest
-
-
-
-
-####### Mit neuer Fun ########
-model = train(makeLearner("regr.km", nugget.estim = TRUE, control = list(trace = FALSE)), makeRegrTask(data = data, target = "ratio"))
+                       ############## SPOT ##################
+# define and fix (-> for all optimzers the same) objective function / simulation 
+model = train(makeLearner("regr.randomForest"), makeRegrTask(data = data, target = "ratio"))
 fun = function(x) {
   df = as.data.frame(t(x))
   colnames(df) <- c("power", "time", "pressure")
   return(getPredictionResponse(predict(model, newdata = df)))
 }
 
-
-
-fun = function(x) {
-  df = as.data.frame(x)
-  return(getPredictionResponse(predict(model, newdata = df)))
-}
-
-
-# alternativ mit objfun
 ps = makeParamSet(
   makeIntegerParam("power", lower = 10, upper = 5555),
   makeIntegerParam("time", lower = 500, upper = 20210),
   makeIntegerParam("pressure", lower = 0, upper = 1000)
 )
-
 
 objfun = makeSingleObjectiveFunction(
   name = "Kapton",
@@ -119,21 +53,55 @@ objfun = makeSingleObjectiveFunction(
   minimize = FALSE
 )
 
-objfun(t(c(1004, 10624	,660)))
-
-
-objfun2 = function(df){
-  df = as.data.frame(t(x))
-  colnames(df) <- c("power", "time", "pressure")
-  return(getPredictionResponse(predict(objfun, newdata = df)))
+# wrapper function; fun3 receives matrix in the right structure
+fun2 = function(xmat1) {
+  apply(xmat1, 1, objfun)
 }
 
 
-objfun2(da1)
+##### 1. ES-Optimizer
 
-da1 <- as.data.frame(c(1004, 10624	,660))
+# define optimizer / algo
+fun3 = function(xmat) {
+  xmat = as.data.frame(t(xmat))
+  colnames(xmat) <- c("nu", "mue")
+  optimES(fun = fun2, lower = c(10,500,0), upper = c(5555,20210,1000), 
+              control = list(funEvals = 50, nu = xmat$nu, mue = xmat$mue))$ybest
+}
 
-da1 = as.data.frame(t(da1))
-colnames(da1) <- c("power", "time", "pressure")
+fun4 = function(xmat2) {
+  apply(xmat2, 1, fun3)
+}
+
+# define metamodel and run spot
+res <- spot(fun = fun4, lower = c(3,5), upper = c(5,15),
+            control = list(funevals = 100))
+
+
+
+
+##### 2. Genoud-Optimizer -> muss nicht optimiert werden, weil nur populationsize als parameter
+##### logischerweise ist er umso höher, je höher populationsize ist
+# define objective function / simulation
+
+# define optimizer / algo
+fun3 = function(x) {
+  optimGenoud(fun = fun2, lower = c(10,500,0), upper = c(5555,20210,1000), 
+              control = list(funEvals = 50, populationSize = x))$ybest
+}
+
+fun4 = function(xmat){
+  apply(xmat,1,fun3)
+}
+
+# define metamodel and run spot
+res <- spot(fun = fun4, lower = c(20), upper = c(40), 
+            control = list(funevals = 100))
+
+res$ybest
+res$xbest
+
+
+
 
 
